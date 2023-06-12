e31e04fe04f28d277311ffdd6fa87a3547e98390
gavizet <gavizet@student.42.fr>
2023-06-12 13:45:39 +0200

    M05E00 done. Add docstring, code cleanup

--- Module_05/ex_00/matrix.py
+++ Module_05/ex_00/matrix.py
@@ -7,6 +7,19 @@ class Matrix:
 
     @staticmethod
     def __check_args(arg) -> None:
+        """Makes sure the args passed at initialization to our Matrix/Vector class is valid
+
+        Args:
+            arg (any): The arg we want to parse and check
+
+        Raises:
+            ValueError: if the arg is not either :
+                - List of lists of equal length filled with int/float
+                - Tuple of 2 positive int/float
+
+        Returns:
+            None : if not error was found while parsing
+        """
         if not isinstance(arg, (list, tuple)):
             raise ValueError(
                 f"Argument has to be a List or a Tuple, not {type(arg)}")
@@ -31,6 +44,16 @@ class Matrix:
 
     @staticmethod
     def __get_data(arg) -> list:
+        """Return the data for our Matrix/Vector instance at __init__
+
+        Args:
+            arg (list, tuple): what we base ourselves on to create the data
+
+        Returns:
+            list: If instance was initialized with a list, just return it.
+                    If it was with a tuple, return a list of the dimensions specificied by
+                    the tuple, and filled with '0.0' values
+        """
         if isinstance(arg, list):
             data = arg
         else:
@@ -41,6 +64,15 @@ class Matrix:
 
     @staticmethod
     def __get_shape(arg) -> tuple:
+        """Return the shape for our Matrix/Vector instance at __init__
+
+        Args:
+            arg (list, tuple): what we base ourselves on to create the shape
+
+        Returns:
+            list: If instance was initialized with a tuple, just return it.
+                If it was with a list, return a tuple that contains the dimensions of that list
+        """
         if isinstance(arg, tuple):
             shape = arg
         else:
@@ -55,12 +87,28 @@ class Matrix:
         self.type = type(self)
 
     def T(self):
+        """Compute the Transpose of our Matrix
+
+        Returns:
+            Vector / Matrix: our new transposed object with the same type as self
+        """
         result = [[self.data[col][row]
                    for col in range(self.shape[0])]
                   for row in range(self.shape[1])]
         return type(self)(result)
 
     def __add__(self, other):
+        """Compute the addition of two Matrices of the same dimension
+
+        Args:
+            other (Matrix): the matrix we want to add to self
+
+        Raises:
+            ValueError: if other is not a matrix or is a matrix of different shape
+
+        Returns:
+            Matrix / Vector: our resulting object with the same type as self
+        """
         if not isinstance(other, Matrix) or self.shape != other.shape:
             raise ValueError("Can only add between 2 Matrix of the same shape")
         result = [[self.data[row][col] + other.data[row][col]
@@ -72,6 +120,17 @@ class Matrix:
         return self.__add__(other)
 
     def __sub__(self, other):
+        """Compute the substraction of two Matrices of the same dimension
+
+        Args:
+            other (Matrix): the matrix we want to sub to self
+
+        Raises:
+            ValueError: if other is not a matrix or is a matrix of different shape
+
+        Returns:
+            Matrix / Vector: our resulting object with the same type as self
+        """
         if not isinstance(other, Matrix) or self.shape != other.shape:
             raise ValueError("Can only add between 2 Matrix of the same shape")
         result = [[self.data[row][col] - other.data[row][col]
@@ -83,6 +142,18 @@ class Matrix:
         return self.__sub__(other)
 
     def __mul__(self, other):
+        """Compute the multiplication of a Matrix by a Scalar/Vector/Matrix with valid dimensions
+
+        Args:
+            other (int / float / Vector / Matrix): the object we want to multiple our Matrix with
+
+        Raises:
+            ValueError: if the shapes of our objects are not compatible for multiplication
+            TypeError: if the type of other is not an int / float / Vector / Matrix
+
+        Returns:
+            Matrix / Vector: our resulting object after multiplication
+        """
         if isinstance(other, (int, float)):
             # Multiply each number of the Matrix by Scalar
             result = [[self.data[row][col] * other
@@ -112,6 +183,17 @@ class Matrix:
         return self.__mul__(other)
 
     def __truediv__(self, other):
+        """Compute the division of a Matrix by a Scalar
+
+        Args:
+            other (int, float): the scalar by which we want to divide self
+
+        Raises:
+            ValueError: if other not an int / float, of it's == 0
+
+        Returns:
+            Matrix / Vector: our resulting object with the same type as self
+        """
         if not isinstance(other, (int, float)):
             raise ValueError("Can only divide with int of float")
         if other == 0:
@@ -143,6 +225,17 @@ class Vector(Matrix):
             raise ValueError("Not a valid vector shape.")
 
     def dot(self, other):
+        """Compute the Dot Product between two vectors
+
+        Args:
+            other (Vector): the other Vector we want to do the dot product with
+
+        Raises:
+            ValueError: if other is not a Vector or its shape is different to self
+
+        Returns:
+            int / float: the result of the dot product
+        """
         if not isinstance(other, Vector) or self.shape != other.shape:
             raise ValueError(
                 "Can only do the dot product with 2 Vectors of the same shape")
