9cc2a2b9901548f2381d8fab5b691d2b7c4f53e4
gavizet <gavizet@student.42.fr>
2023-06-13 12:27:38 +0200

    M05E02 all tests passing. Small modif for style warnings

--- tests/Module_05/test_ex02.py
+++ tests/Module_05/test_ex02.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from Module_05.ex_02.prediction import simple_predict
 
-features = np.arange(1, 6)
+feat = np.arange(1, 6)
 
 THETA = [
     (np.array([5, 0]), np.array([5., 5., 5., 5., 5.])),
@@ -14,14 +14,14 @@ THETA = [
 
 @pytest.mark.parametrize("theta, expected", THETA)
 def test_simple_predict(theta, expected):
-    np.testing.assert_array_equal(simple_predict(features, theta), expected)
+    np.testing.assert_array_equal(simple_predict(feat, theta), expected)
 
 
 ERRORS = [
-    (features, [1., 2., 3.], None),  # arg not nparray
-    (features, np.array(["1", "2"]), None),  # arg not numbers
+    (feat, [1., 2., 3.], None),  # arg not nparray
+    (feat, np.array(["1", "2"]), None),  # arg not numbers
     (np.array([[1, 2], [1, 2]]), np.array([0, 1]), None),  # feature dim not 1
-    (features, np.array([0, 1, 2]), None),  # theta shape not (2, 1)
+    (feat, np.array([0, 1, 2]), None),  # theta shape not (2, 1)
     # (, , None),
 ]
 
@@ -29,4 +29,4 @@ ERRORS = [
 @pytest.mark.parametrize("features, theta, expected", ERRORS)
 def test_simple_predict_errors(features, theta, expected):
     result = simple_predict(features, theta)
-    assert result == None
+    assert result == expected
