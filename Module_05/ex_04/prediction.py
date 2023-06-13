19c15b2b2e482ecb15381bc791ed6234d226f2ec
gavizet <gavizet@student.42.fr>
2023-06-13 12:53:40 +0200

    M05E03 should be done. Need to be tested, but need package creation first to import.

--- Module_05/ex_04/prediction.py
+++ Module_05/ex_04/prediction.py
@@ -1,4 +1,31 @@
+""" Understand and manipulate the notion of hypothesis in machine learning. """
 import numpy as np
+from Module_05.ex_03.tools import add_intercept
+
+
+def _args_are_valid(features, theta) -> bool:
+    """Make sure the parameters are valid for our program
+
+    Args:
+        features (np.ndarray): vector of dimension m * 1
+        theta (np.ndarray): vector of dimension 2 * 1
+
+    Returns:
+        bool: True if features are of the desired type and dimensions, False otherwise
+    """
+    if not isinstance(features, np.ndarray) or not isinstance(theta, np.ndarray):
+        return False
+    if (not np.issubdtype(features.dtype, np.floating) and
+            not np.issubdtype(features.dtype, np.integer)):
+        return False
+    if (not np.issubdtype(theta.dtype, np.floating) and
+            not np.issubdtype(theta.dtype, np.integer)):
+        return False
+    if features.shape not in [(features.size, ), (features.size, 1)]:
+        return False
+    if theta.shape not in [(2, ), (2, 1)]:
+        return False
+    return True
 
 
 def predict_(x, theta):
@@ -17,12 +44,46 @@ def predict_(x, theta):
     Raises:
         This function should not raise any Exceptions.
     """
-    pass
+    if not _args_are_valid(x, theta):
+        return None
+    x_prime = add_intercept(x)
+    y_hat = x_prime.dot(theta)
+    return y_hat
 
 
 def main():
-    pass
+    features_vector = np.arange(1, 6)
+
+    # Test 1
+    param_vector1 = np.array([5, 0])
+    result = predict_(features_vector, param_vector1)
+    print("=============")
+    print("Expected: array([5., 5., 5., 5., 5.])")
+    print(f"Result: {repr(result)}")
+
+    # Test 2
+    param_vector2 = np.array([0, 1])
+    result = predict_(features_vector, param_vector2)
+    print("=============")
+    print("Expected: array([1., 2., 3., 4., 5.])")
+    print(f"Result: {repr(result)}")
+
+    # Test 3
+    param_vector3 = np.array([5, 3])
+    result = predict_(features_vector, param_vector3)
+    print("=============")
+    print("Expected: array([ 8., 11., 14., 17., 20.])")
+    print(f"Result: {repr(result)}")
+
+    # Test 4
+    param_vector4 = np.array([-3, 1])
+    result = predict_(features_vector, param_vector4)
+    print("=============")
+    print("Expected: array([-2., -1., 0., 1., 2.])")
+    print(f"Result: {repr(result)}")
 
 
 if __name__ == "__main__":
     main()
+
+# Pytest test file can be found in tests/Module_05/test_ex04.py
--- tests/Module_05/test_ex04.py
+++ tests/Module_05/test_ex04.py
@@ -1,23 +1,31 @@
 import pytest
 import numpy as np
-from Module_05.ex_04.prediction import predict
+from Module_05.ex_04.prediction import predict_
 
-PARAMS = [
-    (),
+feat = np.arange(1, 6)
+
+THETA = [
+    (np.array([5, 0]), np.array([5., 5., 5., 5., 5.])),
+    (np.array([0, 1]), np.array([1., 2., 3., 4., 5.])),
+    (np.array([5, 3]), np.array([8., 11., 14., 17., 20.])),
+    (np.array([-3, 1]), np.array([-2., -1., 0., 1., 2.])),
 ]
 
 
-@pytest.mark.parametrize("x, theta, expected", PARAMS)
-def test_add_intercept(x, theta, expected):
-    np.testing.assert_array_equal(predict(x, theta), expected)
+@pytest.mark.parametrize("theta, expected", THETA)
+def test_simple_predict(theta, expected):
+    np.testing.assert_array_equal(predict_(feat, theta), expected)
 
 
 ERRORS = [
-    (),
+    (feat, [1., 2., 3.], None),  # arg not nparray
+    (feat, np.array(["1", "2"]), None),  # arg not numbers
+    (np.array([[1, 2], [1, 2]]), np.array([0, 1]), None),  # feature dim not 1
+    (feat, np.array([0, 1, 2]), None),  # theta shape not (2, 1)
 ]
 
 
 @pytest.mark.parametrize("x, theta, expected", ERRORS)
 def test_simple_predict_errors(x, theta, expected):
-    result = predict(x, theta)
+    result = predict_(x, theta)
     assert result == expected
