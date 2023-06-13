ec0b73f0da6af9626831b9866c03d68b0763adca
gavizet <gavizet@student.42.fr>
2023-06-13 11:40:31 +0200

    M05E03 start

---
+++ Module_05/ex_03/tools.py
@@ -0,0 +1,28 @@
+""" Understand and manipulate the notion of hypothesis in machine learning.
+You must implement a function which adds an extra column of '1' on the left side 
+of a given vector or matrix """
+import numpy as np
+
+
+def add_intercept(array):
+    """Adds a column of '1' to the non-empty numpy.array x.
+    Args:
+        array: has to be a numpy.array of dimension m * n.
+
+    Returns:
+        X (numpy.array): of dimension m * (n + 1).
+        None if x is not a numpy.array.
+        None if x is an empty numpy.array.
+
+    Raises:
+        This function should not raise any Exception.
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
