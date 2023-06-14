b2699d24572d7fba327d2785715f9a05575d4bc0
gavizet <gavizet@student.42.fr>
2023-06-14 11:02:34 +0200

    M05E06 done and tested.

--- Module_05/ex_06/loss.py
+++ Module_05/ex_06/loss.py
@@ -2,6 +2,31 @@ import numpy as np
 from Module_05.ex_04.prediction import predict_
 
 
+def _args_are_valid(y, y_hat) -> bool:
+    """Make sure the parameters are valid for our program
+
+    Args:
+        y - features (np.ndarray): vector of dimension m * 1
+        y_hat - theta (np.ndarray): vector of dimension m * 1
+
+    Returns:
+        bool: True if features are of the desired type and dimensions, False otherwise
+    """
+    if not isinstance(y, np.ndarray) or not isinstance(y_hat, np.ndarray):
+        return False
+    if y.size == 0 or y.shape != y_hat.shape:
+        return False
+    if (not np.issubdtype(y.dtype, np.floating) and
+            not np.issubdtype(y.dtype, np.integer)):
+        return False
+    if (not np.issubdtype(y_hat.dtype, np.floating) and
+            not np.issubdtype(y_hat.dtype, np.integer)):
+        return False
+    if y.shape not in [(y.size, ), (y.size, 1)]:
+        return False
+    return True
+
+
 def loss_elem_(y: np.ndarray, y_hat: np.ndarray) -> np.ndarray or None:
     """ Calculates all the elements (y_pred - y)^2 of the loss function.
 
@@ -17,7 +42,10 @@ def loss_elem_(y: np.ndarray, y_hat: np.ndarray) -> np.ndarray or None:
     Raises:
         This function should not raise any Exception.
     """
-    pass
+    if not _args_are_valid(y, y_hat):
+        return None
+    J_elem = np.square(y_hat - y)
+    return J_elem
 
 
 def loss_(y: np.ndarray, y_hat: np.ndarray) -> float or None:
@@ -35,12 +63,46 @@ def loss_(y: np.ndarray, y_hat: np.ndarray) -> float or None:
     Raises:
         This function should not raise any Exception.
     """
-    pass
+    if not _args_are_valid(y, y_hat):
+        return None
+    # This is almost equivalent to the Mean Squared Error loss function, except we divide by 2
+    elem_num = len(y)
+    J_value = np.sum(loss_elem_(y, y_hat)) / (elem_num * 2)
+    return J_value
 
 
 def main():
-    pass
+    x1 = np.array([[0.], [1.], [2.], [3.], [4.]])
+    theta1 = np.array([[2.], [4.]])
+    y_hat1 = predict_(x1, theta1)
+    y1 = np.array([[2.], [7.], [12.], [17.], [22.]])
+
+    # Example 1:
+    print(f"Result: {repr(loss_elem_(y1, y_hat1))}")
+    print(f"Expected: {repr(np.array([[0.], [1], [4], [9], [16]]))}")
+    print("===============")
+
+    # Example 2:
+    print(f"Result: {loss_(y1, y_hat1)}")
+    print("Expected: 3.0")
+    print("===============")
+
+    x2 = np.array([0, 15, -9, 7, 12, 3, -21]).reshape(-1, 1)
+    theta2 = np.array([[0.], [1.]]).reshape(-1, 1)
+    y_hat2 = predict_(x2, theta2)
+    y2 = np.array([2, 14, -13, 5, 12, 4, -19]).reshape(-1, 1)
+
+    # Example 3:
+    print(f"Result: {loss_(y2, y_hat2)}")
+    print("Expected: 2.142857142857143")
+    print("===============")
+
+    # Example 4: perfect model, no error
+    print(f"Result: {loss_(y2, y2)}")
+    print("Expected: 0.0")
 
 
 if __name__ == "__main__":
     main()
+
+# Pytest test file with complete test battery can be found in tests/Module_05/test_ex06.py
