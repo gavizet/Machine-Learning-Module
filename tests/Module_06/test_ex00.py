2a74a421a5ee2a0ef90fcc0954171fc85e61ac72
gavizet <gavizet@student.42.fr>
2023-06-15 13:44:31 +0200

    M6E0 tests start

---
+++ tests/Module_06/test_ex00.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+from Module_06.ex_00.gradient import simple_gradient
+
+ERRORS = [
+    (),
+]
+
+
+@pytest.mark.parametrize("args, expected", ERRORS)
+def test_errors_gradient(args, expected):
+    pass
+
+
+GRADIENTS = [
+    (),
+]
+
+
+@pytest.mark.parametrize("args, expected", GRADIENTS)
+def test_errors_gradient(args, expected):
+    pass
