a0e13cb9b72e41ebd5b999242265d7b6f6983b4c
gavizet <gavizet@student.42.fr>
2023-06-15 13:48:32 +0200

    M6E0 main() creation

--- Module_06/ex_00/gradient.py
+++ Module_06/ex_00/gradient.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 
+def _args_are_valid(x, y, theta):
+    pass
+
+
 def simple_gradient(x, y, theta):
     """ Computes a gradient vector from three non-empty numpy.array, with a for-loop.
         The three arrays must have compatible shapes.
@@ -20,3 +24,26 @@ def simple_gradient(x, y, theta):
         This function should not raise any Exception.
     """
     pass
+
+
+def main():
+    x = np.array([12.4956442, 21.5007972, 31.5527382,
+                 48.9145838, 57.5088733]).reshape((-1, 1))
+    y = np.array([37.4013816, 36.1473236, 45.7655287,
+                 46.6793434, 59.5585554]).reshape((-1, 1))
+    print(repr(x))
+    print(repr(y))
+
+    # Example 0:
+    theta1 = np.array([2, 0.7]).reshape((-1, 1))
+    print(repr(theta1))
+    simple_gradient(x, y, theta1)
+    # Output: array([[-19.0342574], [-586.66875564]])
+    # Example 1:
+    theta2 = np.array([1, -0.4]).reshape((-1, 1))
+    simple_gradient(x, y, theta2)
+    # Output: array([[-57.86823748], [-2230.12297889]])
+
+
+if __name__ == "__main__":
+    main()
