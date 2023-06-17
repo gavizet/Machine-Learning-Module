8e777d92d271cd99c105adaf22b725fe5eb9dc4b
gavizet <gavizet@student.42.fr>
2023-06-17 09:48:43 +0200

    M6E3 loss_elem_ done

--- Module_06/ex_03/my_linear_regression.py
+++ Module_06/ex_03/my_linear_regression.py
@@ -57,6 +57,12 @@ class MyLinearRegression():
         y_hat = x_prime.dot(self.thetas)
         return y_hat
 
+    def loss_elem_(self, y: np.ndarray, y_hat: np.ndarray) -> np.ndarray | None:
+        if not self._args_are_valid_ndarrays(y, y_hat):
+            return None
+        elem_loss = np.square(y_hat - y)
+        return elem_loss
+
     def fit_(self, x: np.ndarray, y: np.ndarray) -> np.ndarray | None:
         if not self._args_are_valid_ndarrays(x, y):
             return None
