0865f216cbe1e7b05d20debcf8149e7a4756b1d9
gavizet <gavizet@student.42.fr>
2023-06-22 11:48:28 +0200

    M7E3 done and tested.

--- Module_07/ex_03/gradient.py
+++ Module_07/ex_03/gradient.py
@@ -30,16 +30,25 @@ def gradient(x: np.ndarray, y: np.ndarray, theta: np.ndarray) -> np.ndarray | No
     """ Computes a gradient vector from three non-empty numpy.array.
 
     Args:
-        x (numpy.ndarray): matrix m * n, 
-        y (numpy.ndarray): vector m * 1.
-        theta (numpy.ndarray): vector (n + 1) * 1.
+        x (numpy.ndarray): matrix m * n, training examples, m = num of values, n = num of features
+        y (numpy.ndarray): vector m * 1, predicted values
+        theta (numpy.ndarray): vector (n + 1) * 1, our parameters
 
     Return:
-        gradient (numpy.ndarray): vector of dimensions n * 1, 
+        gradient (numpy.ndarray): vector (n + 1) * 1
         containg the result of the formula for all J (loss).
         None if y or y_hat are not of the required dimensions or type.
     """
-    pass
+    # Add column of 1s left of Matrix so we can vectorize the equation
+    # (we just scale theta_0 by 1). x_prime is of dimension m * n + 1
+    x_prime = np.c_[np.ones((x.shape[0], 1)), x]
+    # Get predicted values, vector m * 1
+    y_hat = x.dot(theta)
+    # Transpose X' so it is now (n + 1) * m
+    x_transpose = np.transpose(x_prime)
+    # Do the dot product between X'T (n + 1, m) and vector (m, 1). Resulting shape is (n + 1, 1)
+    gradient_vector = x_transpose.dot(y_hat - y) / x_prime.shape[0]
+    return gradient_vector
 
 
 def tests():
@@ -55,13 +64,19 @@ def tests():
     theta1 = np.array([3, 0.5, -6]).reshape((-1, 1))
 
     # Example 1:
-    gradient(x, y, theta1)
-    # expected = np.array([[ -33.71428571], [ -37.35714286], [183.14285714], [-393.]])
+    result = gradient(x, y, theta1)
+    expected = np.array(
+        [[-33.71428571], [-37.35714286], [183.14285714], [-393.]])
+    np.testing.assert_array_almost_equal(result, expected)
 
     # Example 2:
     theta2 = np.array([0, 0, 0]).reshape((-1, 1))
-    gradient(x, y, theta2)
-    # expected = np.array([[ -0.71428571], [ 0.85714286], [23.28571429], [-26.42857143]])
+    result = gradient(x, y, theta2)
+    expected = np.array([[-0.71428571], [0.85714286],
+                        [23.28571429], [-26.42857143]])
+    np.testing.assert_array_almost_equal(result, expected)
+
+    print("All tests passing, no assert raised, gg")
 
 
 if __name__ == "__main__":
